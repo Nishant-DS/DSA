9608dc6c41fc38260b2b3fef8be935bcdb21dc7d
Nishant-DS <40952133+Nishant-DS@users.noreply.github.com>
2025-04-14 19:53:51 +0530

    Update EqualPointInAStringOfBrackets.py

--- EqualPointInAStringOfBrackets.py
+++ EqualPointInAStringOfBrackets.py
@@ -1,19 +1,21 @@
 def findIndex(str):
     opening_braces = closing_braces = 0
     n = len(str)
-
     for i in range(n):
-        if str[i] == ")":
+        if str[i] == ')':
             closing_braces += 1
     
     for i in range(n):
         if opening_braces == closing_braces:
             return i
-        if str[i] == '(':
+        if str[i] == ')':
+            closing_braces -= 1
+        
+        elif str[i] == '(':
             opening_braces += 1
         
-        elif str[i] == ')':
-            closing_braces -= 1
+    if opening_braces == closing_braces:
+        return n
     
     return -1
 
